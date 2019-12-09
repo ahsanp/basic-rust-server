26c351eb14622ad643c0b9523dd02b3fe6612984
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-12-09 14:18:39 -0600

    Make a single threaded server with simple requests

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,24 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection Made!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get_request = "GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if String::from_utf8_lossy(&buffer).starts_with(&get_request) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 400 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}\r\n\r\n{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
