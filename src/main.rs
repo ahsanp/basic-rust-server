ef377338db0294259bad47dd910f17816a63b0fd
Ahsan Pervaiz <ahsan.pervaiz.anjum@gmail.com>
2019-12-09 14:07:31 -0600

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connection Made!");
+    }
+}
